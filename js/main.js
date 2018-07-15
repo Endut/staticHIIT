58d8d1902715c9760fc3b20c9fc7bf977c454114
Endut <adam.a.juraszek@googlemail.com>
2018-07-16 00:22:49 +0200

    remove preset dir + update json service

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const indicator = document.getElementById("indicator");
 const countdownIndicator = document.getElementById("countdownIndicator");
 const presetSelector = document.getElementById("presetSelector");
 
-const presetDataURL = "https://api.myjson.com/bins/bbdra";
+const presetDataURL = "https://api.myjson.com/bins/jevl2";
 
 // buttons
 // const pauseButton = document.getElementById("pause");
@@ -142,63 +142,6 @@ function setCountdownIndicator(text) {
 
 function setCountdownIndicatorColor(color) {
 	countdownIndicator.style.color = color;
-};
-
-
-function Timer(callback, delay) {
-    var timerId, countdownTimer, start, remaining = delay;
-
-
-    this.pause = function() {
-        clearTimeout(timerId);
-        countdownTimer.pause();
-        remaining -= new Date() - start;
-    };
-
-    this.resume = function() {
-        start = new Date();
-        clearTimeout(timerId);
-        timerId = setTimeout(callback, remaining);
-        countdownTimer = new CountdownTimer(Math.floor(remaining/1000));
-    };
-
-    this.clearAll = function() {
-    	clearTimeout(timerId);
-    	countdownTimer.clearAll();
-    }
-
-    this.resume();
-};
-
-
-function CountdownTimer(totalSeconds) {
-    var timerId, remaining = totalSeconds;
-
-    this.pause = function() {
-        clearInterval(timerId);
-    };
-
-    this.resume = function() {
-        clearInterval(timerId);
-        timerId = setInterval(function() {
-        	if (remaining > 0) {
-        		remaining -= 1;
-        		setCountdownIndicator(remaining);  
-        		if (remaining < 5) {
-        			setCountdownIndicatorColor("red");
-        			audio.play('countin');
-        		} else {
-        			setCountdownIndicatorColor("black");
-        		}     		
-        	}
-        }, 1000);
-    };
-
-    this.clearAll = function() {
-    	clearInterval(timerId);
-    }
-
-    this.resume();
 };
 
 
